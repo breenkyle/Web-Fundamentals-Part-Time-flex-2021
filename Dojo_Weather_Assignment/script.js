c2a2ded64faa1a6031273d4871f4c28639392b27
Kyle Breen <87739246+breenkyle@users.noreply.github.com>
2021-10-17 01:06:44 -0400

    Add files via upload

---
+++ Dojo_Weather_Assignment/script.js
@@ -0,0 +1,33 @@
+console.log("Weather is Wonderful!");
+
+function message() {
+    alert(`Loading weather report...`)
+}
+
+let accept = document.querySelector('#cookie-policy')
+function acceptCookies() {
+    accept.remove()
+}
+
+// attempted to get the temperatures to change when the slector was changed, but could not manage it.//
+
+
+
+// var inputCelcius = document.getElementById("#inputCelcius");
+// function changeTemp(){
+//     ((inputCelcius*1.8)+32);
+// }
+
+
+// var inputCelcius = document.querySelector(valNum);
+
+// function changeTemp(){
+//     document.getElementById("inputCelcius").innerHTML = (valNum-32) / 1.8;
+
+// }
+
+// function changeTemp(valNum){
+//     document.getElementsByClassName("highTemp").innerHTML=(valNum*1.8)=32;
+// }
+
+
