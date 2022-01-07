b183cb1a278ae5b08d8d9b5ede385597054c86c7
Kyle Breen <87739246+breenkyle@users.noreply.github.com>
2022-01-07 14:11:50 -0500

    Add files via upload
    Dojo_Survey

---
+++ dojo_survey/server.py
@@ -0,0 +1,31 @@
+from flask import Flask, session, redirect, request, render_template
+
+
+app = Flask(__name__)
+app.secret_key = "This is the Way"
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/process', methods=['POST'])
+def process():
+    session['name'] = request.form['name']
+    session['location'] = request.form['location']
+    session['language'] = request.form['language']
+    session['comments'] = request.form['comments']
+    return redirect('/result')
+
+
+@app.route('/result')
+def result():
+    name = session['name']
+    location = session['location']
+    language = session['language']
+    comments = session['comments']
+    return render_template('results.html', name = name, location = location, language = language, comments = comments)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
